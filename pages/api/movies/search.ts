0fffb858593d99793126ea22f0bbe2d5761e7d1e
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2022-12-26 10:02:16 +0100

    Add instructions to run the code locally

--- pages/api/movies/search.ts
+++ pages/api/movies/search.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSessionByToken } from '../../../database/sessions';
+import { checkSearch } from '../../../utils/connect_to_python';
 import { validateTokenFromCsrfSecret } from '../../../utils/csrf';
 import { searchFromDjango } from '../../../utils/getFromDjango';
 
@@ -32,6 +33,10 @@ export default async function handler(
       .json({ errors: { message: 'Invalid CSRF token, you hacker!' } });
   }
   const searchItem = request.body.searchItem;
+
+  // if you want to use this application locally, comment out the following line of code and remove line 41
+  // const data = JSON.parse(await checkSearch(searchItem));
+
   // get the data by connecting to external Django API
   const data = await searchFromDjango(searchItem);
   if (!data) {
